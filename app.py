a6859317e53025e98ecbdc803bbf116b7366aa1a
Akku <akashsureshmyd@gmail.com>
2020-12-27 20:31:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,8 +44,7 @@ def respond():
     if text[0:9] == "/yt_music":
         yt_image,dir,capy=yt_music(text[10:])
         bot.sendChatAction(chat_id=chat_id, action="typing")
-        bot.sendMessage(chat_id=chat_id, text=capy, reply_to_message_id=msg_id)
-        #bot.send_audio(audio=dir,thumb=yt_image,caption=capy,chat_id=chat_id,reply_to_message_id=msg_id)
+        bot.send_audio(audio=open(dir,'rb'),thumb=yt_image,caption=capy,chat_id=chat_id,reply_to_message_id=msg_id)
         #for files in (dir, yt_image):
             #if files and os.path.exists(files):
                 #os.remove(files)
