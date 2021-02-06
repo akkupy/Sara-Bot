655f6e6f201d4b25b9b306f399a054fe4c2f9b60
Akku <akashsureshmyd@gmail.com>
2021-02-06 10:01:36 +0530

    Update

--- Modules/Wiki.py
+++ Modules/Wiki.py
@@ -2,25 +2,28 @@ import wikipediaapi
 import wikipedia
 
 def wiki(name):
-    global Sub
-    Sub=name
+    try:
+        global Sub
+        Sub = name
 
-    #Wiki Language
-    wiki_wiki = wikipediaapi.Wikipedia('en')
+        # Wiki Language
+        wiki_wiki = wikipediaapi.Wikipedia('en')
 
-    #Wiki Search
-    page_py = wiki_wiki.page(Sub)
+        # Wiki Search
+        page_py = wiki_wiki.page(Sub)
 
-    #Check if page is there
-    Exist=page_py.exists()
-    if Exist is True:
-        # Page Title
-        Title = page_py.title
-        # Content
-        Content = Title + '\n' + wikipedia.summary(Sub, sentences=5)+'\n'+page_py.fullurl
-        return Content
-    else:
-        return 'Not Found On Wiki'
+        # Check if page is there
+        Exist = page_py.exists()
+        if Exist is True:
+            # Page Title
+            Title = page_py.title
+            # Content
+            Content = Title + '\n' + wikipedia.summary(Sub, sentences=5) + '\n' + page_py.fullurl
+            return Content
+        else:
+            return 'Not Found On Wiki'
+    except:
+        return "Wiki Encountered an Error! Try Again"
 
 
 
--- Modules/Yt_music.py
+++ Modules/Yt_music.py
@@ -6,47 +6,49 @@ import youtube_dl
 
 
 def yt_music(song_name):
-    videosSearch = VideosSearch(song_name, limit=1)
-
-    result = videosSearch.result()
-    first_result = result["result"]
-    yt_url = first_result[0]["link"]
-    yt_title = first_result[0]["title"]
-    yt_pub_time = first_result[0]["publishedTime"]
-    yt_id = first_result[0]["id"]
-    yt_duration = first_result[0]["duration"]
-    yt_image = f"https://img.youtube.com/vi/{yt_id}/hqdefault.jpg"
-
-    if not os.path.isdir("./Downloads/"):
-        os.makedirs("./Downloads/")
-
-    yt_image = wget.download(yt_image, out="./Downloads/")
-
-    if not os.path.isdir("./music/"):
-        os.makedirs("./music/")
-    yt_song = (
-            f'youtube-dl --force-ipv4 -q -o "./music/{yt_title}.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 128k '
-            + yt_url
-    )
-
-    os.system(yt_song)
     try:
-        a=glob.glob("./music/*.webm")
-        b=a[0]
-        c=b[8:]
+        videosSearch = VideosSearch(song_name, limit=1)
+
+        result = videosSearch.result()
+        first_result = result["result"]
+        yt_url = first_result[0]["link"]
+        yt_title = first_result[0]["title"]
+        yt_pub_time = first_result[0]["publishedTime"]
+        yt_id = first_result[0]["id"]
+        yt_duration = first_result[0]["duration"]
+        yt_image = f"https://img.youtube.com/vi/{yt_id}/hqdefault.jpg"
+
+        if not os.path.isdir("./Downloads/"):
+            os.makedirs("./Downloads/")
+
+        yt_image = wget.download(yt_image, out="./Downloads/")
+
+        if not os.path.isdir("./music/"):
+            os.makedirs("./music/")
+        yt_song = (
+                f'youtube-dl --force-ipv4 -q -o "./music/{yt_title}.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 128k '
+                + yt_url
+        )
+
+        os.system(yt_song)
+        try:
+            a = glob.glob("./music/*.webm")
+            b = a[0]
+            c = b[8:]
+        except:
+            a = glob.glob("./music/*.mp3")
+            b = a[0]
+            c = b[8:]
+        dir = f"./music/{c}"
+        dir1 = f"./music/{c}"
+        capy = f"**Song Name ➠** `{yt_title}` \n**Published On ➠** `{yt_pub_time}` \n**Duration ➠** `{yt_duration}` \n**Link ➠** `{yt_url}`"
+        if os.path.exists(dir):
+            return yt_image, dir, capy
+        elif os.path.exists(dir1):
+            return yt_image, dir1, capy
+        else:
+            return "Somethings Wrong!", "Please Try Again", "ee"
+
     except:
-        a = glob.glob("./music/*.mp3")
-        b = a[0]
-        c = b[8:]
-    dir = f"./music/{c}"
-    dir1 = f"./music/{c}"
-    capy = f"**Song Name ➠** `{yt_title}` \n**Published On ➠** `{yt_pub_time}` \n**Duration ➠** `{yt_duration}` \n**Link ➠** `{yt_url}`"
-    if os.path.exists(dir):
-        return yt_image, dir, capy
-    elif os.path.exists(dir1):
-        return yt_image, dir1, capy
-    else:
-        return "Somethings Wrong!","Please Try Again" ,"ee"
-
-
+        return "Please Try Again"
 
