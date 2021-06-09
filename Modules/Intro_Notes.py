8907d18ce18a84e5c2ad0d3c57c2342a8bab2e73
Akash S <akashsureshmyd@gmail.com>
2021-06-09 14:10:54 +0530

    Update Intro_Notes.py

--- Modules/Intro_Notes.py
+++ Modules/Intro_Notes.py
@@ -7,7 +7,7 @@
 #/__/    \__\ |_| \_\ |_| \_\  \___/
 #
 # Copyright of Akash, 2021          
-# https://www.github.com/AkkuPY     
+# https://www.github.com/akkupy    
 # https://t.me/Akku_Legend   
 
 
